e72ffab6f705c37466fd3cce7098fdfb2c208072
plonsdale1 <petex.1995@outlook.com>
2019-08-27 20:09:10 +0000

    timeout1

---
+++ timer1.js
@@ -0,0 +1,14 @@
+// node timer1.js 10 3 5 15 9 ! what it has to do !
+ 
+let arr = process.argv.slice(2);
+
+
+
+  for(let num of arr) {
+    setTimeout(
+      () => {
+    process.stdout.write('\x07');
+    // console.log("nfjfnjnfkj\n")
+  }, num * 1000)
+}
+  
